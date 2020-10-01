c3bd14368fcbcab8977e6be9397df432e514789f
dogfooter <dogfooter219@gmail.com>
2020-10-02 00:17:23 +0900

    zsh powerline theme

--- mac/mac-primary.sh
+++ mac/mac-primary.sh
@@ -1,17 +1,17 @@
 #!/bin/bash 
 
 # brew stuff
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+# /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew tap caskroom/cask 
-brew install mas-cli/tap/mas 
+# brew install mas-cli/tap/mas 
 
 brew install -y $(cat brew-list.txt)
 brew cask install $(cat brew-cask-list.txt)
-mas install $(cat brew-mas-list.txt | cut -d' ' -f1)
+# mas install $(cat brew-mas-list.txt | cut -d' ' -f1)
 
 # Install Monaco powerline font
-git clone https://github.com/DogFooter/powerline-fonts
-cp ./powerline-fonts/Monaco/Monaco\ for\ Powerline.otf
+# git clone https://github.com/DogFooter/powerline-fonts
+# cp ./powerline-fonts/Monaco/Monaco\ for\ Powerline.otf
 
 # oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
--- zsh-syn-high.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/zsh
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
-echo "source ${(q-)PWD}/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc
